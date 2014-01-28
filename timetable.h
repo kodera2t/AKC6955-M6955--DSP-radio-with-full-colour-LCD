c0b89f6e79ecc8fe0bc2ae6f2e21dcbce192f21c
kodera2t <kodera2t@yahoo.co.jp>
2014-01-28 18:09:08 +0900

    Create timetable.h

---
+++ timetable.h
@@ -0,0 +1,202 @@
+#define st_num  96
+#define sw_num  4
+#include <avr/pgmspace.h>
+prog_char s_string_0[] PROGMEM = "CRI       : 1044kHz";//0
+prog_char s_string_1[] PROGMEM = "CRI       : 5980kHz";
+prog_char s_string_2[] PROGMEM = "VOI       : 7220kHz";
+prog_char s_string_3[] PROGMEM = "VOA       : 7580kHz";
+prog_char s_string_4[] PROGMEM = "VOA       :12150kHz";//1
+prog_char s_string_5[] PROGMEM = "VOA       :15215kHz";
+prog_char s_string_6[] PROGMEM = "                   ";
+prog_char s_string_7[] PROGMEM = "                   ";
+prog_char s_string_8[] PROGMEM = "                   ";//2
+prog_char s_string_9[] PROGMEM = "                   ";
+prog_char s_string_10[] PROGMEM = "                   ";
+prog_char s_string_11[] PROGMEM = "                   ";
+prog_char s_string_12[] PROGMEM = "                   ";//3
+prog_char s_string_13[] PROGMEM = "                   ";
+prog_char s_string_14[] PROGMEM = "                   ";
+prog_char s_string_15[] PROGMEM = "                   ";
+prog_char s_string_16[] PROGMEM = "                   ";//4
+prog_char s_string_17[] PROGMEM = "                   ";
+prog_char s_string_18[] PROGMEM = "                   ";
+prog_char s_string_19[] PROGMEM = "                   ";
+prog_char s_string_20[] PROGMEM = "                   ";//5
+prog_char s_string_21[] PROGMEM = "                   ";
+prog_char s_string_22[] PROGMEM = "                   ";
+prog_char s_string_23[] PROGMEM = "                   ";
+prog_char s_string_24[] PROGMEM = "VOK       : 7580kHz";//6
+prog_char s_string_25[] PROGMEM = "IRIB      : 5965kHz";
+prog_char s_string_26[] PROGMEM = "IRIB      : 7365kHz";
+prog_char s_string_27[] PROGMEM = "                   ";
+prog_char s_string_28[] PROGMEM = "VOV: 9840, 12020kHz";//7
+prog_char s_string_29[] PROGMEM = "VOA:7365,7425,11860";
+prog_char s_string_30[] PROGMEM = "RTI       :11605kHz";
+prog_char s_string_31[] PROGMEM = "CRI       : 7440kHz";
+prog_char s_string_32[] PROGMEM = "CRI       : 9435kHz";//8
+prog_char s_string_33[] PROGMEM = "VOK       : 7580kHz";
+prog_char s_string_34[] PROGMEM = "VOA:5830,7365,11860";
+prog_char s_string_35[] PROGMEM = "VOA       : 9900kHz";
+prog_char s_string_36[] PROGMEM = "                   ";//9
+prog_char s_string_37[] PROGMEM = "                   ";
+prog_char s_string_38[] PROGMEM = "                   ";
+prog_char s_string_39[] PROGMEM = "                   ";
+prog_char s_string_40[] PROGMEM = "RAE       : 6060kHz";//10
+prog_char s_string_41[] PROGMEM = "RAE       :11710kHz";
+prog_char s_string_42[] PROGMEM = "KBS       : 9580kHz";
+prog_char s_string_43[] PROGMEM = "VOA: 9845, 15130kHz";
+prog_char s_string_44[] PROGMEM = "KBS       :11810kHz";//11
+prog_char s_string_45[] PROGMEM = "                   ";
+prog_char s_string_46[] PROGMEM = "                   ";
+prog_char s_string_47[] PROGMEM = "                   ";
+prog_char s_string_48[] PROGMEM = "                   ";//12
+prog_char s_string_49[] PROGMEM = "                   ";
+prog_char s_string_50[] PROGMEM = "                   ";
+prog_char s_string_51[] PROGMEM = "                   ";
+prog_char s_string_52[] PROGMEM = "                   ";//13
+prog_char s_string_53[] PROGMEM = "                   ";
+prog_char s_string_54[] PROGMEM = "                   ";
+prog_char s_string_55[] PROGMEM = "                   ";
+prog_char s_string_56[] PROGMEM = "                   ";//14
+prog_char s_string_57[] PROGMEM = "                   ";
+prog_char s_string_58[] PROGMEM = "                   ";
+prog_char s_string_59[] PROGMEM = "                   ";
+prog_char s_string_60[] PROGMEM = "                   ";//15
+prog_char s_string_61[] PROGMEM = "                   ";
+prog_char s_string_62[] PROGMEM = "                   ";
+prog_char s_string_63[] PROGMEM = "                   ";
+prog_char s_string_64[] PROGMEM = "                   ";//16
+prog_char s_string_65[] PROGMEM = "                   ";
+prog_char s_string_66[] PROGMEM = "                   ";
+prog_char s_string_67[] PROGMEM = "                   ";
+prog_char s_string_68[] PROGMEM = "VOK:3250,7580,9650k";//17
+prog_char s_string_69[] PROGMEM = "KBS       : 6155kHz";
+prog_char s_string_70[] PROGMEM = "WHARA     : 9930kHz";
+prog_char s_string_71[] PROGMEM = "RTI       :11605kHz";
+prog_char s_string_72[] PROGMEM = "VOK       : 7580kHz";//18
+prog_char s_string_73[] PROGMEM = "KBS       : 9805kHz";
+prog_char s_string_74[] PROGMEM = "                   ";
+prog_char s_string_75[] PROGMEM = "                   ";
+prog_char s_string_76[] PROGMEM = "CRI       : 7440kHz";//19
+prog_char s_string_77[] PROGMEM = "VOK:3250,7580,9650k";
+prog_char s_string_78[] PROGMEM = "KBS       : 9805kHz";
+prog_char s_string_79[] PROGMEM = "VOM       :12085kHz";
+prog_char s_string_80[] PROGMEM = "CRI       : 7260kHz";//20
+prog_char s_string_81[] PROGMEM = "RTI       : 9735kHz";
+prog_char s_string_82[] PROGMEM = "BBC :9740, 11895kHz";
+prog_char s_string_83[] PROGMEM = "VOV: 9840, 12020kHz";
+prog_char s_string_84[] PROGMEM = "VOV: 9840, 12020kHz";//21
+prog_char s_string_85[] PROGMEM = "BBC       : 5875kHz";
+prog_char s_string_86[] PROGMEM = "VOA       : 9370kHz";
+prog_char s_string_87[] PROGMEM = "VOR       : 5980kHz";
+prog_char s_string_88[] PROGMEM = "IRIB      : 9585kHz";//22
+prog_char s_string_89[] PROGMEM = "RTW       : 9390kHz";
+prog_char s_string_90[] PROGMEM = "RTI       : 9735kHz";
+prog_char s_string_91[] PROGMEM = "BBC       : 6195kHz";
+prog_char s_string_92[] PROGMEM = "VOV: 9840, 12020kHz";//23
+prog_char s_string_93[] PROGMEM = "                   ";
+prog_char s_string_94[] PROGMEM = "CRI       : 7210kHz";
+prog_char s_string_95[] PROGMEM = "                   ";
+
+
+
+PROGMEM const char *sw_stations[] =
+{   
+  s_string_0,
+  s_string_1,
+  s_string_2,
+  s_string_3,
+  s_string_4,
+  s_string_5,
+  s_string_6,
+  s_string_7,
+  s_string_8,
+  s_string_9,
+  s_string_10,
+  s_string_11,
+  s_string_12,
+  s_string_13,
+  s_string_14,
+  s_string_15,
+  s_string_16,
+  s_string_17,
+  s_string_18,
+  s_string_19,
+  s_string_20,
+  s_string_21,
+  s_string_22,
+  s_string_23,
+  s_string_24,
+  s_string_25,
+  s_string_26,
+  s_string_27,
+  s_string_28,
+  s_string_29,
+  s_string_30,
+  s_string_31,
+  s_string_32,
+  s_string_33,
+  s_string_34,
+  s_string_35,
+  s_string_36,
+  s_string_37,
+  s_string_38,
+  s_string_39,
+  s_string_40,
+  s_string_41,
+  s_string_42,
+  s_string_43,
+  s_string_44,
+  s_string_45,
+  s_string_46,
+  s_string_47,
+  s_string_48,
+  s_string_49,
+  s_string_50,
+  s_string_51,
+  s_string_52,
+  s_string_53,
+  s_string_54,
+  s_string_55,
+  s_string_56,
+  s_string_57,
+  s_string_58,
+  s_string_59,
+  s_string_60,
+  s_string_61,
+  s_string_62,
+  s_string_63,
+  s_string_64,
+  s_string_65,
+  s_string_66,
+  s_string_67,
+  s_string_68,
+  s_string_69,
+  s_string_70,
+  s_string_71,
+  s_string_72,
+  s_string_73,
+  s_string_74,
+  s_string_75,
+  s_string_76,
+  s_string_77,
+  s_string_78,
+  s_string_79,
+  s_string_80,
+  s_string_81,
+  s_string_82,
+  s_string_83,
+  s_string_84,
+  s_string_85,
+  s_string_86,
+  s_string_87,
+  s_string_88,
+  s_string_89,
+  s_string_90,
+  s_string_91,
+  s_string_92,
+  s_string_93,
+  s_string_94,
+  s_string_95,
+  };
+ 
